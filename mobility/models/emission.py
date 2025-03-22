f1229f26baf98b9ef7188458f415d498bbbcdfa4
Bouchra (ghost) <anonymous_student@users.noreply.github.com>
2025-03-22 23:18:06 +0100

    modif

--- mobility/models/emission.py
+++ mobility/models/emission.py
@@ -1,23 +1,18 @@
 from mobility.db import get_db
+from mobility import create_app
 
-def aeroport_info(iata_code):
-    """Retourne les informations des vols partant d'un aéroport donné."""
+app = create_app()
+
+def aeroport_info(iata_code): 
     db = get_db()
-    result = db.execute("""
-        SELECT 
-            dep_airport.name AS "Aéroport de départ",
-            arr_airport.name AS "Aéroport d'arrivée",
-            arr_airport.latitude_deg AS "Latitude arrivée",
-            arr_airport.longitude_deg AS "Longitude arrivée",
-            aircraft.aircraft_type AS "Type d'appareil"
-        FROM flight
-        JOIN airport AS dep_airport ON flight.iata_departure = dep_airport.iata_code
-        JOIN airport AS arr_airport ON flight.iata_arrival = arr_airport.iata_code
-        JOIN aircraft ON flight.iata_aircraft = aircraft.iata_aircraft
-        WHERE dep_airport.iata_code = ?;
-    """, (iata_code,)).fetchall()  
-    
-    return result
+    return db.execute("""SELECT dep_airport.name AS "Aeroport de depart",arr_airport.name AS "Aeroport d'arrivee",.latitude_deg AS "Latitude arrivée",arr_airport.longitude_deg AS "Longitude arrivée",aircraft.aircraft_type AS "Type d'appareil"
+                        FROM flight
+                        JOIN airport AS dep_airport ON flight.iata_departure = dep_airport.iata_code
+                        JOIN airport AS arr_airport ON flight.iata_arrival = arr_airport.iata_code
+                        JOIN aircraft ON flight.iata_aircraft = aircraft.iata_aircraft
+                        WHERE dep_airport.iata_code = ?""",(iata_code, iata_code)).fetchall()
+
+
 flights_from_bru = aeroport_info("BRU")
 flights_from_crl = aeroport_info("CRL")
 flights_from_lgg = aeroport_info("LGG")
